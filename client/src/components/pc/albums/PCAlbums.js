091eee38c387ed30f9903e39a4220ffecbd06ab5
ParanoiaSun <1369664817@qq.com>
2019-08-27 08:48:44 +0800

    some animation

--- client/src/components/pc/albums/PCAlbums.js
+++ client/src/components/pc/albums/PCAlbums.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './albums.css';
+
 
 class PCAlbums extends React.Component {
     render() {
         return(
             <div className="pc-albums-wrapper">
-                hi, pc albums
+                <div className="albums-cover-wrapper">
+                    <div className="albums-cover">
+
+                    </div>
+                    <div className="albums-cover">
+
+                    </div>
+                    <div className="albums-cover">
+
+                    </div>
+                    <div className="albums-cover">
+
+                    </div>
+                    <div className="albums-cover">
+
+                    </div>
+                </div>
             </div>
         );
     }
