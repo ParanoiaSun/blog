5122dc909e0f434a4dcfc3a98aacdf2f43521646
ParanoiaSun <1369664817@qq.com>
2019-09-24 09:59:05 +0800

    Updates

---
+++ precache-manifest.40cd279c09eaf0a9972df9e1d1506048.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405a605e060c883e6be837183a48f00d",
+    "url": "/index/index.html"
+  },
+  {
+    "revision": "f40ea7c20a00a2612c83",
+    "url": "/index/static/css/main.57ae4a2b.chunk.css"
+  },
+  {
+    "revision": "0fa5ed34845cf64ec22c",
+    "url": "/index/static/js/2.f24daa96.chunk.js"
+  },
+  {
+    "revision": "f40ea7c20a00a2612c83",
+    "url": "/index/static/js/main.b9a8a2c4.chunk.js"
+  },
+  {
+    "revision": "cdfb4d66ac5c648b0ce6",
+    "url": "/index/static/js/runtime~main.5ebec3f0.js"
+  },
+  {
+    "revision": "ae6db4f97acb8cb351142ae9c51f2704",
+    "url": "/index/static/media/blog-logo.ae6db4f9.png"
+  },
+  {
+    "revision": "1c727a7d8ae31ae9a30ca09e249217d0",
+    "url": "/index/static/media/iconfont.1c727a7d.eot"
+  },
+  {
+    "revision": "26f9e0b313ea8e771a0488dfcb0a64f8",
+    "url": "/index/static/media/iconfont.26f9e0b3.ttf"
+  },
+  {
+    "revision": "a30da54c974b233d428193a7553f7f86",
+    "url": "/index/static/media/iconfont.a30da54c.svg"
+  },
+  {
+    "revision": "e8cf98e64c43afe309591ec9e6e167b2",
+    "url": "/index/static/media/iconfont.e8cf98e6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.221ec1d395c225798f154764b3122de1.js"
+  "/index/precache-manifest.40cd279c09eaf0a9972df9e1d1506048.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
