7d2c23b30724afe6d31813e0a153c96766c77b43
ParanoiaSun <1369664817@qq.com>
2019-09-30 16:04:39 +0800

    子留言动态发送

--- client/src/components/pc/messages/PCMessageDetail.js
+++ client/src/components/pc/messages/PCMessageDetail.js
@@ -1,14 +1,54 @@
 import React from 'react';
 import './messages.css';
 
+function isEmpty(input) {
+    return input + '' === 'null' || input + '' === 'undefined' || input.trim ? input.trim() === '' : input.replace(/^\s+|\s+$/gm) === '';
+}
+
 class PCMessageDetail extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
             message: props.message,
-            index: props.index
+            subMessage: (props.message.sub_message + '' === 'undefined') ? [] : props.message.sub_message,
+            index: props.index,
+            sendSubInputName: '',
+            sendSubInputContent: '',
+            sendInputDisplay: false,
         }
+    }
+
+    handleSendReply() {
+        if(isEmpty(this.state.sendSubInputName) || isEmpty(this.state.sendSubInputContent)) {
+            console.log('请完善回复信息^_^');
+        } else {
+            this.setState({
+                subMessage: [{
+                    content: this.state.sendSubInputContent,
+                    name: this.state.sendSubInputName,
+                    send_time: '2019-09-30 12:13:14'
+                }, ...this.state.subMessage]
+            })
+        }
+    }
+
+    handleReply() {
+        this.setState({
+            sendInputDisplay: !this.state.sendInputDisplay
+        });
+    }
+
+    handleSendSubNameChange(e) {
+        this.setState({
+            sendSubInputName: e.target.value
+        })
+    }
+
+    handleSendSubContentChange(e) {
+        this.setState({
+            sendSubInputContent: e.target.value
+        })
     }
 
     render() {
@@ -25,12 +65,29 @@ class PCMessageDetail extends React.Component {
                 <div className="pc-message-detail-content">
                     {this.state.message.content}
                 </div>
-                <div className="pc-message-detail-reply">
-                    回复
-                </div>
-                {(this.state.message.sub_message === undefined) ? null :
+                {
+                    this.state.sendInputDisplay ? <div className="pc-sub-messages-send-wrapper appear-div">
+                        <div className="pc-sub-messages-send-name-input">
+                            <input onChange={this.handleSendSubNameChange.bind(this)} type="text" placeholder="你的昵称"/>
+                        </div>
+                        <div className="pc-sub-messages-send-content-input">
+                            <textarea onChange={this.handleSendSubContentChange.bind(this)} rows="4" placeholder="回复内容"/>
+                        </div>
+                        <div className="pc-sub-messages-send-area">
+                            <div onClick={this.handleReply.bind(this)} className="pc-sub-messages-close-button">
+                                收起
+                            </div>
+                            <div onClick={this.handleSendReply.bind(this)} className="pc-sub-messages-send-button">
+                                发送
+                            </div>
+                        </div>
+                    </div> : <div onClick={this.handleReply.bind(this)} className="pc-message-detail-reply">
+                        回复
+                    </div>
+                }
+                {(this.state.subMessage === undefined || this.state.subMessage.length === 0) ? null :
                     <div className="pc-sub-message-detail-wrapper">
-                        {this.state.message.sub_message.map((item, key) => {
+                        {this.state.subMessage.map((item, key) => {
                         return <div className="pc-sub-message-detail-content" key={ key }>
                                 <div className="pc-message-detail-title">
                                     <div className="pc-message-detail-name">
