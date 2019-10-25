50b0b70769cc9c9e8c1606a6bf10e9bdd86bacdb
ParanoiaSun <1369664817@qq.com>
2019-10-25 14:29:59 +0800

    上线

--- README.md
+++ README.md
@@ -6,5 +6,9 @@
 
 API文档：[南浦旧铺API文档][1]
 
-[1]: https://github.com/ParanoiaSun/blog/blob/master/%E5%8D%97%E6%B5%A6%E6%97%A7%E9%93%BAAPI%E6%96%87%E6%A1%A3.md
+上线地址：[http://www.nampo.cn][2]
 
+2019.10.25 部署完毕，第一次正式上线
+
+[1]: https://github.com/ParanoiaSun/blog/blob/master/%E5%8D%97%E6%B5%A6%E6%97%A7%E9%93%BAAPI%E6%96%87%E6%A1%A3.md
+[2]: http://www.nampo.cn
---
+++ client/build/precache-manifest.b7c7ffb883ab8f707409eeac55b7f214.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99ad0e35becd9b9403d20cb90da668e",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "3e8c3c259d58376ace82",
+    "url": "/blog/static/css/main.4645238f.chunk.css"
+  },
+  {
+    "revision": "45d44d71d9d3c1c022fc",
+    "url": "/blog/static/js/2.7b81e0cc.chunk.js"
+  },
+  {
+    "revision": "3e8c3c259d58376ace82",
+    "url": "/blog/static/js/main.7f024c4b.chunk.js"
+  },
+  {
+    "revision": "a96de44e3e4e80549926",
+    "url": "/blog/static/js/runtime~main.8f4415e6.js"
+  },
+  {
+    "revision": "ae6db4f97acb8cb351142ae9c51f2704",
+    "url": "/blog/static/media/blog-logo.ae6db4f9.png"
+  },
+  {
+    "revision": "3b98d3fa62979ff13ff420b0df20e591",
+    "url": "/blog/static/media/iconfont.3b98d3fa.woff"
+  },
+  {
+    "revision": "88fc6faf70d82db70de862b75766e502",
+    "url": "/blog/static/media/iconfont.88fc6faf.ttf"
+  },
+  {
+    "revision": "9e90b97af0bd3077f7661b9b8cba48fe",
+    "url": "/blog/static/media/iconfont.9e90b97a.svg"
+  },
+  {
+    "revision": "d7b57173b25d5e4ac5731b9ac4b0b134",
+    "url": "/blog/static/media/iconfont.d7b57173.eot"
+  }
+]);
